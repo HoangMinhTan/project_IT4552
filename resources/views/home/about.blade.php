80c4d39b411d6b45ebc3471857ad2d874bb25132
Huy Hieu <huyhieugckm@gmail.com>
2020-12-20 22:20:11 +0700

    Modify homepage

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,13 +1,18 @@
-
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
 <!DOCTYPE html>
 <html lang="zxx">
 
 <head>
-	<title>Downy Shoes</title>
-	<!-- custom-theme -->
+	<title>Downy Shoes an Ecommerce Category Bootstrap Responsive Website Template | About :: w3layouts</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="Downy Shoes" />
+	<meta name="keywords" content="Downy Shoes Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 	<script type="application/x-javascript">
 		addEventListener("load", function () {
 			setTimeout(hideURLbar, 0);
@@ -19,6 +24,7 @@
 	</script>
 	<!-- //custom-theme -->
 	<link href="../resources/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="stylesheet" href="../resources/css/about.css" type="text/css" media="screen" property="" />
 	<link rel="stylesheet" href="../resources/css/shop.css" type="text/css" media="screen" property="" />
 	<link href="../resources/css/style7.css" rel="stylesheet" type="text/css" media="all" />
 	<link href="../resources/css/style.css" rel="stylesheet" type="text/css" media="all" />
@@ -32,11 +38,10 @@
 
 <body>
 	<!-- banner -->
-	<div class="banner_top" id="home">
+<div class="banner_top innerpage" id="home">
 		<div class="wrapper_top_w3layouts">
-
 			<div class="header_agileits">
-				<div class="logo">
+				<div class="logo inner_page_log">
 					<h1><a class="navbar-brand" href="index.html"><span>Downy</span> <i>Shoes</i></a></h1>
 				</div>
 				<div class="overlay overlay-contentpush">
@@ -48,8 +53,7 @@
 							<li><a href="about.html">About</a></li>
 							<li><a href="404.html">Team</a></li>
 							<li><a href="shop.html">Shop Now</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li><a href="{{'login'}}">Login</a></li>
+							<li><a href="contact.html">Contact</a></li>
 						</ul>
 					</nav>
 				</div>
@@ -65,156 +69,167 @@
 							<button class="top_shoe_cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
 						</form>
 					</div>
-
 				</div>
-				<!-- //cart details -->
-				<!-- search -->
-				<div class="search_w3ls_agileinfo">
-					<div class="cd-main-header">
-						<ul class="cd-header-buttons">
-							<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
-						</ul>
-					</div>
-					<div id="cd-search" class="cd-search">
-						<form action="#" method="post">
-							<input name="Search" type="search" placeholder="Click enter after typing...">
-						</form>
-					</div>
-				</div>
-				<!-- //search -->
-
-				<div class="clearfix"></div>
 			</div>
-			<!-- /slider -->
-			<div class="slider">
-				<div class="callbacks_container">
-					<ul class="rslides callbacks callbacks1" id="slider4">
+		</div>
+		<!-- //cart details -->
+		<!-- search -->
+		<div class="search_w3ls_agileinfo">
+			<div class="cd-main-header">
+				<ul class="cd-header-buttons">
+					<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+				</ul>
+			</div>
+			<div id="cd-search" class="cd-search">
+				<form action="#" method="post">
+					<input name="Search" type="search" placeholder="Click enter after typing...">
+				</form>
+			</div>
+		</div>
+		<!-- //search -->
+		<div class="clearfix"></div>
+		<!-- /banner_inner -->
+		<div class="services-breadcrumb_w3ls_agileinfo">
+			<div class="inner_breadcrumb_agileits_w3">
 
-						<li>
-							<div class="banner-top2">
-								<div class="banner-info-wthree">
-									<h3>Nike</h3>
-									<p>See how good they feel.</p>
+				<ul class="short">
+					<li><a href="index.html">Home</a><i>|</i></li>
+					<li>About</li>
+				</ul>
+			</div>
+		</div>
+		<!-- //banner_inner -->
+	</div>
 
-								</div>
-
-							</div>
-						</li>
-						<li>
-							<div class="banner-top3">
-								<div class="banner-info-wthree">
-									<h3>Heels</h3>
-									<p>For All Walks of Life.</p>
-
-								</div>
-
-							</div>
-						</li>
-						<li>
-							<div class="banner-top">
-								<div class="banner-info-wthree">
-									<h3>Sneakers</h3>
-									<p>See how good they feel.</p>
-
-								</div>
-
-							</div>
-						</li>
-						<li>
-							<div class="banner-top1">
-								<div class="banner-info-wthree">
-									<h3>Adidas</h3>
-									<p>For All Walks of Life.</p>
-
-								</div>
-
-							</div>
-						</li>
-					</ul>
+	<!-- //banner -->
+	<!-- top Products -->
+	<div class="ads-grid_shop">
+		<div class="shop_inner_inf">
+			<h3 class="head">About Us</h3>
+			<p class="head_para">Add Some Description</p>
+			<div class="inner_section_w3ls">
+				<div class="col-md-6 news-left">
+					<img src="../resources/images/ab.jpg" alt=" " class="img-responsive">
+				</div>
+				<div class="col-md-6 news-right">
+					<h4>Welcome to our Downy Shoes</h4>
+					<p class="sub_p">Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet ultrices. Maecenas dictum arcu purus, sit amet
+						volutpat purus viverra sit amet. Quisque lacinia quam sed tortor interdum, malesuada congue nunc ornare. Cum sociis
+						In semper lorem eget tortor pulvinar ultricies.
+					</p>
+					<p>Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet ultrices. Maecenas dictum arcu purus, sit amet
+						volutpat purus viverra sit amet. Quisque lacinia quam sed tortor interdum, malesuada congue nunc ornare. Cum sociis
+						. In semper lorem eget tortor pulvinar ultricies.
+					</p>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
-			<!-- //slider -->
-			<ul class="top_icons">
-				<li><a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
-				<li><a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
-				<li><a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a></li>
-				<li><a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a></li>
+		</div>
 
-			</ul>
-		</div>
 	</div>
-	<!-- //banner -->
-	<!-- /girds_bottom-->
-	<div class="grids_bottom">
-		<div class="style-grids">
-			<div class="col-md-6 style-grid style-grid-1">
-				<img src="../resources/images/b1.jpg" alt="shoe">
-			</div>
-		</div>
-		<div class="col-md-6 style-grid style-grid-2">
-			<div class="style-image-1_info">
-				<div class="style-grid-2-text_info">
-					<h3>Nike DOWNSHIFTER</h3>
-					<p>Nike Shoes .</p>
-					<div class="shop-button">
-						<a href="shop.html">Shop Now</a>
+	<div class="mid_services">
+		<div class="col-md-6 according_inner_grids">
+			<h3 class="heading two">Who We Are</h3>
+			<div class="according_info">
+				<div class="panel-group about_panel" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingOne">
+							<h4 class="panel-title asd">
+								<a class="pa_italic" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
+								    aria-controls="collapseOne">
+							  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>assumenda est cliche voluptate
+							</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+							<div class="panel-body panel_text">
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+								cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div class="style-image-2">
-				<img src="../resources/images/b2.jpg" alt="shoe">
-				<div class="style-grid-2-text">
-					<h3>Air force</h3>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	</div>
-	<!-- //grids_bottom-->
-	<!-- /girds_bottom2-->
-	<div class="grids_sec_2">
-		<div class="style-grids_main">
-			<div class="col-md-6 grids_sec_2_left">
-				<div class="grid_sec_info">
-					<div class="style-grid-2-text_info">
-						<h3>Sneakers</h3>
-						<p>Sneaker Shoes.</p>
-						<div class="shop-button">
-							<a href="shop.html">Shop Now</a>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+							<h4 class="panel-title asd">
+								<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
+								    aria-controls="collapseTwo">
+							  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Itaque earum rerum
+							</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							<div class="panel-body panel_text">
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+								cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+							<h4 class="panel-title asd">
+								<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false"
+								    aria-controls="collapseThree">
+							  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>autem accusamus terry qui
+							</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+							<div class="panel-body panel_text">
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+								cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+							<h4 class="panel-title asd">
+								<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false"
+								    aria-controls="collapseThree">
+							  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>autem accusamus terry qui
+							</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+							<div class="panel-body panel_text">
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+								cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="style-image-2">
-					<img src="../resources/images/b4.jpg" alt="shoe">
-					<div class="style-grid-2-text">
-						<h3>Air force</h3>
-					</div>
-				</div>
 			</div>
-			<div class="col-md-6 grids_sec_2_left">
 
-				<div class="style-image-2">
-					<img src="../resources/images/b3.jpg" alt="shoe">
-					<div class="style-grid-2-text">
-						<h3>Air force</h3>
+		</div>
+		<div class="col-md-6 mid_services_img">
+			<div class="bar-grids">
+				<h3 class="heading two three">Our Skills</h3>
+				<div class="skill_info">
+					<h6>Development<span> 95% </span></h6>
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped active" style="width: 95%">
+						</div>
 					</div>
-				</div>
-				<div class="grid_sec_info last">
-					<div class="style-grid-2-text_info">
-						<h3>Sneakers</h3>
-						<p>Sneakers Shoes .</p>
-						<div class="shop-button two">
-							<a href="shop.html">Shop Now</a>
+					<h6>Pricing<span> 85% </span></h6>
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped active" style="width: 85%">
+						</div>
+					</div>
+					<h6>Production <span>90% </span></h6>
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped active" style="width: 90%">
+						</div>
+					</div>
+					<h6>Advertising <span>86% </span></h6>
+					<div class="progress prgs-last">
+						<div class="progress-bar progress-bar-striped active" style="width: 86%">
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="clearfix"></div>
+
 		</div>
+		<div class="clearfix"> </div>
 	</div>
-	<!-- //grids_bottom2-->
 	<!-- /Properties -->
 	<div class="mid_slider_w3lsagile">
 		<div class="col-md-3 mid_slider_text">
@@ -327,6 +342,7 @@
 		<div class="clearfix"></div>
 	</div>
 	<!-- //newsletter-->
+
 	<!-- footer -->
 	<div class="footer_agileinfo_w3">
 		<div class="footer_inner_info_w3ls_agileits">
@@ -429,15 +445,31 @@
 			</div>
 			<div class="clearfix"></div>
 
-			<p class="copy-right-w3ls-agileits">&copy 2018 Downy Shoes. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			<p class="copy-right-w3ls-agileits">&copy 2018 Downy Shoes. All rights reserved | Design by <a href="http://w3layouts.com/">w3layouts</a></p>
 		</div>
 	</div>
 	</div>
 	<!-- //footer -->
-    <a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 	<!-- js -->
 	<script type="text/javascript" src="../resources/js/jquery-2.1.4.min.js"></script>
 	<!-- //js -->
+	<!-- cart-js -->
+	<script src="../resources/js/minicart.js"></script>
+	<script>
+		shoe.render();
+
+		shoe.cart.on('shoe_checkout', function (evt) {
+			var items, len, i;
+
+			if (this.subtotal() > 0) {
+				items = this.items();
+
+				for (i = 0, len = items.length; i < len; i++) {}
+			}
+		});
+	</script>
+	<!-- //cart-js -->
 	<!-- /nav -->
 	<script src="../resources/js/modernizr-2.6.2.min.js"></script>
 	<script src="../resources/js/classie.js"></script>
@@ -459,28 +491,33 @@
 		});
 	</script>
 	<!-- //cart-js -->
-	<!--search-bar-->
-	<script src="../resources/js/search.js"></script>
-	<!--//search-bar-->
-	<script src="../resources/js/responsiveslides.min.js"></script>
+	<!-- script for responsive tabs -->
+	<script src="../resources/js/easy-responsive-tabs.js"></script>
 	<script>
-		$(function () {
-			$("#slider4").responsiveSlides({
-				auto: true,
-				pager: true,
-				nav: true,
-				speed: 1000,
-				namespace: "callbacks",
-				before: function () {
-					$('.events').append("<li>before event fired.</li>");
-				},
-				after: function () {
-					$('.events').append("<li>after event fired.</li>");
+		$(document).ready(function () {
+			$('#horizontalTab').easyResponsiveTabs({
+				type: 'default', //Types: default, vertical, accordion           
+				width: 'auto', //auto or any width like 600px
+				fit: true, // 100% fit in a container
+				closed: 'accordion', // Start closed if in accordion view
+				activate: function (event) { // Callback function if tab is switched
+					var $tab = $(this);
+					var $info = $('#tabInfo');
+					var $name = $('span', $info);
+					$name.text($tab.text());
+					$info.show();
 				}
+			});
+			$('#verticalTab').easyResponsiveTabs({
+				type: 'vertical',
+				width: 'auto',
+				fit: true
 			});
 		});
 	</script>
-	<!-- js for portfolio lightbox -->
+	<!--search-bar-->
+	<script src="../resources/js/search.js"></script>
+	<!--//search-bar-->
 	<!-- start-smoth-scrolling -->
 	<script type="text/javascript" src="../resources/js/move-top.js"></script>
 	<script type="text/javascript" src="../resources/js/easing.js"></script>
@@ -495,7 +532,6 @@
 		});
 	</script>
 	<!-- //end-smoth-scrolling -->
-
 	<script type="text/javascript" src="../resources/js/bootstrap-3.1.1.min.js"></script>
 
 
