4d8dff78d442469f9496f6e3ec8178de254a5e90
Huy Hieu <huyhieugckm@gmail.com>
2020-12-21 19:21:09 +0700

    Modify Payment

--- resources/views/home/shop.blade.php
+++ resources/views/home/shop.blade.php
@@ -8,7 +8,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <html lang="zxx">
 
 <head>
-	<title>Downy Shoes an Ecommerce Category Bootstrap Responsive Website Template | Shop :: w3layouts</title>
+	<title>Shop</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="keywords" content="Downy Shoes Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
